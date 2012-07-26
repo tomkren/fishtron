9de3001e859e9f468aabf5842209b41967c3a9f5
Tom Kren <tomkren@gmail.com>
2012-07-26 22:22:58 +0200

    FF.hs - simulation probably almost done
    winning is still uncompleted - for now there is one fish sufficient to
    solve the thing
    it has to be massivelly tested...

--- FF.hs
+++ FF.hs
@@ -2,32 +2,116 @@ module FF where
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.List
+import Data.Char
 import Data.Maybe
 import Util
 import Debug.Trace
 
 
 l1 = mkSea [
+ "                     ",
+ "         b           ",
  "                      ",
- "          b           ",
- "                      ",
- "       aaaaa          ",
+ "      aaaaa          ",
  "       11           ",
- "        $              ",
+ "       $$              ",
  "                      ",
  "                      ",
  "                      ",
  "                      "
  ]
 
-lvl0@(Sea l0_posMap l0_obMap l0_moving l0_rec l0_fid _) = mkSea [
+l2 = mkSea [
+  "                      ",
+  "      bbb             ",
+  "      b                ",       
+  "       11             ",
+  "                      ",
+  "                      ",
+  "                      ",
+  "                      ",
+  "                      "
+  ]
+
+l3 = mkSea [
+  "        kk             ",
+  "        aa              ",
+  "        a              ",
+  "        aa11          ",
+  "        a$            ",       
+  "        az             ",
+  "        a             ",
+  "        a$$             ",
+  "        ay            ",
+  "        axx              ",
+  "        aa              ",
+  "                      ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l4 = mkSea [
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "       11a           ",
+  "       aaa             ",
+  "       22a             ",
+  "                    ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l5 = mkSea [
+  "        c            ",
+  "                       ",
+  "       aaa             ",
+  "       22a             ",
+  "        b            ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l6 = mkSea [
+  "                       ",
+  "       aaa             ",
+  "       22a             ",
+  "         b            ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l7 = mkSea [
+  "                       ",
+  "       aaa             ",
+  "       22a             ",
+  "          b            ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l8 = mkSea [
+  "                       ",
+  "                       ",
+  "       aaa A             ",
+  "       11a A             ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l9 = mkSea [
+  "                      ",
+  "                 222  ",
+  "           AAAAA 222  ",
+  "       11  a   b      ",
+  "$$$$$$$$$$$$$$$$$$$$$$"
+  ]
+
+l0@(Sea l0_posMap l0_obMap l0_moving l0_rec l0_fid _) = mkSea [
  "        s                ",
  "       ss            g   ",
  "     $ saaaa             ",
  "        b                ",
  "        cc$$       ffff  ",
- "        d       eeef$    ",
- "   h            eee 11   ",
+ "        d       eeef11     ",
+ "   h            eee     ",
  "   h    33               ",
  "   h    w                ",
  "  $$$$$$$$$$$$$$$$$$$$$  ",
@@ -95,10 +179,10 @@ data Sea = Sea
  , getFishes     :: Fishes
  , getGameStatus :: GameStatus }
 
-data ObType = ObFix | ObStd | ObFish FishType deriving (Show)
-data GameStatus = Normal | GameOver | YouWin
+data ObType = ObFix | ObStd | ObSteel | ObFish FishType deriving (Show)
+data GameStatus = Normal | GameOver | YouWin deriving (Eq)
 
-data FishType = SmallFish | BigFish deriving (Show)
+data FishType = SmallFish | BigFish | DeadFish deriving (Show)
 
 data MovingStatus = Mov | MayMov | Stop
 
@@ -123,28 +207,147 @@ type Shape = [[ HLine ]]
 type HLine = (Int,Int)
 
 data Dir = DUp | DDown | DLeft | DRight deriving (Show,Eq)
-data Cmd = U | D | L | R | Us Int | Ds Int | Ls Int | Rs Int
+data Cmd = S | U | D | L | R | Us Int | Ds Int | Ls Int | Rs Int
+
+
+
+-- fish STEPing --
+
+steps :: Sea -> [Cmd] -> [Sea]
+steps sea cmds = fishSteps sea $ concatMap toDir cmds 
+
+fishSteps :: Sea -> [Either () Dir] -> [Sea]
+fishSteps sea ds 
+ | isGameOver sea = [sea]
+ | otherwise = case ds of
+  [] -> fallSteps sea
+  (dir:dirs) ->
+   let seas = fallSteps sea
+       lSea = last seas
+       sea' = fishStep lSea dir
+    in seas ++ ( if isGameOver lSea then [] else fishSteps sea' dirs  )
+
+
+fishStep :: Sea -> Either () Dir -> Sea
+fishStep sea@(Sea posMap obMap [] rec fishes@(fishId:restFishes) status) eDir = 
+  case eDir of
+   Left _ -> Sea posMap obMap [] rec (restFishes++[fishId]) status
+   Right dir ->
+    case getMovables dir maps fishId of
+     Nothing -> sea
+     Just pusheds -> 
+      let maps'@(posMap',obMap') = foldr (move dir) maps pusheds
+          moving'                = mkMoving maps'   -- TODO    U N E F F E C T I V E
+          ( obMap'' , status' )  = updateKilling dir maps' status pusheds fishes
+          status''               = if (status' == Normal) && (isGoal obMap'' rec fishId) 
+                                   then YouWin else status' 
+       in Sea posMap' obMap'' moving' rec fishes status'' 
+ where
+  maps = (posMap,obMap)
+
+getMovables :: Dir -> Maps -> ObId -> Maybe [ObId]
+getMovables dir maps@(_,obMap) fishId = 
+  let (normal,walls,fishes) = halfDeepNeigbors2 dir maps fishId  
+   in case (walls,fishes) of
+    ([],[fishId']) -> 
+     if (fishId == fishId') && (checkForSteel fishId normal )   
+      then Just (fishId:normal) 
+      else Nothing
+    _ -> Nothing
+ where
+  checkForSteel :: ObId -> [ObId] -> Bool
+  checkForSteel fishId oids = 
+   ( isBigFish' obMap fishId ) || ( and $ map ( not . (isSteel' obMap) ) oids ) 
+
+
+updateKilling :: Dir -> Maps -> GameStatus -> [ObId] -> [ObId] -> (ObMap,GameStatus)
+updateKilling dir maps@(_,obMap) status pusheds fishes 
+  | dir == DUp || dir == DDown = (obMap,status)
+  | otherwise                  = foldr f (obMap,status) fishes
+ where 
+ f :: ObId -> (ObMap,GameStatus) -> (ObMap,GameStatus)
+ f fishId acc@( obMap , status ) = 
+   if ( isSpineKilled maps pusheds fishId ) || ( isSteelKilled maps fishId )
+    then ( kill fishId obMap , GameOver )   
+    else acc
+
+isSpineKilled :: Maps -> [ObId] -> ObId -> Bool
+isSpineKilled maps pusheds fishId =
+  let (onSpine,_,_) = neighbors2 DUp maps fishId
+      onSpine'      = onSpine `intersect` pusheds
+      onSpine''     = filter (not . (isFixedByWall maps)) onSpine'
+   in not $ null onSpine''
+
+isSteelKilled :: Maps -> ObId -> Bool
+isSteelKilled maps@(_,obMap) fishId =
+ let (aboveSpine,_,_) = halfDeepNeigbors2 DUp maps fishId
+     steels           = filter (isSteel' obMap) aboveSpine
+     badSteels        = filter (not . (isFixedByWall maps)) steels
+  in not $ null badSteels
+
+isFixedByWall :: Maps -> ObId -> Bool
+isFixedByWall maps oid =  not $ null walls 
+ where (_,walls,_) = halfDeepNeigbors2 DDown maps oid
+
+
+{--
+updateWinning :: Maps -> Rectangle -> GameStatus -> Fishes -> (Maps,GameStatus,Fishes)
+updateWinning maps@(posMap,obMap) rec status fishes@(fishId:restFishes) 
+ | (status == Normal) && (isGoal obMap rec fishId) = 
+   let 
+
+   bude potřeba předelat mkMoving kvuli odebírání oběktů ze scény
+   protože je tam to [1..size] pač se mkMovingvolá při každym pohnutí
+
+   současný winninig stačí libovolná rybka venku 
+--}
+
+isGoal :: ObMap -> Rectangle -> ObId -> Bool
+isGoal obMap rec fishId =
+  or $ map (rectangleTouched rec) fishPoses 
+ where 
+  fishPoses = posesOfOb $ getOb obMap fishId 
+
+  rectangleTouched :: Rectangle -> Pos -> Bool
+  rectangleTouched ((x1,y1),(x2,y2)) (x,y) 
+   |  x1 >= x  = True
+   |  y1 >= y  = True
+   |  x2 <= x  = True
+   |  y2 <= y  = True
+   | otherwise = False 
+  
+
 
 
 -- FALL STEPing --
 
+
+
 fallSteps :: Sea -> [Sea]
-fallSteps sea = case getMoving sea of
- [] -> [sea]
- _  ->  sea : (fallSteps $ fallStep sea)
+fallSteps sea 
+ | isGameOver sea = [sea]
+ | otherwise = case getMoving sea of
+  [] -> [sea]
+  _  ->  sea : (fallSteps $ fallStep sea)
 
 fallStep :: Sea -> Sea
-fallStep (Sea posMap obMap moving rec fishes status) = 
- let maps@(posMap',obMap') = foldr (move DDown) (posMap,obMap) moving 
-     moving'               = updateMoving maps moving
-     status'               = status -- TODO
-     stoppeds              = moving \\ moving'
-     pxs                   = map (pxByObId obMap)
-  in trace (show ( pxs stoppeds ,
-                   pxs $ getKilledFishes maps stoppeds)) 
-   $ Sea posMap' obMap' moving' rec fishes status' 
+fallStep (Sea posMap obMap moving rec fishes status) =  
+   let maps'@(posMap',obMap') = foldr (move DDown) (posMap,obMap) moving 
+       moving'                = updateMoving maps' moving
+       stoppeds               = moving \\ moving'
+       killedFishes           = getKilledFishes2 maps' stoppeds
+       status'                = if null killedFishes then status else GameOver -- status -- TODO
+       obMap''                = foldr kill obMap' killedFishes
+    in trace (show ( pxs stoppeds , pxs killedFishes )) 
+     $ Sea posMap' obMap'' moving' rec fishes status' 
+ where
+  pxs = map (pxByObId obMap) 
 
-move :: Dir -> ObId -> Maps -> (PosMap,ObMap)
+
+kill :: ObId -> ObMap -> ObMap
+kill fishId obMap = Map.adjust killFish fishId obMap
+
+move :: Dir -> ObId -> Maps -> Maps
 move dir oid (posMap,obMap) = ( posMap'' , obMap' )
  where
   obMap'        = Map.adjust ( adjustPos dir ) oid obMap 
@@ -183,6 +386,15 @@ getKilledFishes maps@(_,obMap) stoppeds
   fishes = filter (isFish' obMap) $ dns
 
 
+getKilledFishes2 :: Maps -> [ObId] -> [ObId]
+getKilledFishes2 maps
+  = concatMap getKilledFishes2' 
+ where
+  getKilledFishes2' :: ObId -> [ObId]
+  getKilledFishes2' stopped =
+   let (_,walls,fishes) = halfDeepNeigbors2 DDown maps stopped
+    in if null walls then fishes else []
+ 
 -- neighbors --
 
 deepNeigbors :: Dir -> Maps -> [ObId] -> [ObId]
@@ -196,8 +408,6 @@ deepNeigbors dir maps@(_,obMap) oids
        childs' = childs \\ acc
     in deepNeigbors' dir maps (x:acc) (childs'++xs)
 
-
-
 halfDeepNeigbors :: Dir -> Maps -> [ObId] -> [ObId]
 halfDeepNeigbors dir maps@(_,obMap) oids 
   = deepNeigbors' dir maps [] oids
@@ -206,13 +416,75 @@ halfDeepNeigbors dir maps@(_,obMap) oids
   deepNeigbors' _   _    acc []     = acc
   deepNeigbors' dir maps acc (x:xs) = 
    let childs  = neighbors dir maps x
-       childs' = filter (not . isWallOrFish' obMap) $ childs \\ acc
-    in deepNeigbors' dir maps (x:acc) (childs'++xs)
+       childs' =  childs \\ acc
+       childs'' 
+        | isWallOrFish' obMap x = filter (not . isWallOrFish' obMap) $ childs'
+        | otherwise = childs' 
+    in deepNeigbors' dir maps (x:acc) (childs''++xs)
 
 neighbors :: Dir -> Maps -> ObId -> [ObId]
 neighbors dir (posMap,obMap) oid
   = nub . catMaybes . map (\pos-> Map.lookup pos posMap ) $ 
     getSigPoses dir (fromJust $ Map.lookup oid obMap ) 
+
+neighbors2 :: Dir -> Maps -> ObId -> ([ObId],[ObId],[ObId])
+neighbors2 dir maps@(_,obMap) oid = obSort obMap $ neighbors dir maps oid
+
+obSort :: ObMap -> [ObId] -> ([ObId],[ObId],[ObId])
+obSort obMap = foldr f ([],[],[])
+ where
+  f oid (ns,ws,fs) = case getObType ob of
+    ObStd    -> ( oid:ns ,     ws ,     fs )
+    ObSteel  -> ( oid:ns ,     ws ,     fs )
+    ObFix    -> (     ns , oid:ws ,     fs )
+    ObFish _ -> (     ns ,     ws , oid:fs )
+   where ob = getOb obMap oid 
+
+{--
+halfDeepNeigbors3 :: Dir -> Maps -> ObId -> ([ObId],[ObId],[ObId]) -- (normal,walls,fishes)
+halfDeepNeigbors3 dir maps@(_,obMap) oid 
+  = deepNeigbors' dir maps ([],[],[]) [oid]
+ where
+  deepNeigbors' :: Dir -> Maps -> ([ObId],[ObId],[ObId]) -> [ObId] -> ([ObId],[ObId],[ObId])
+  deepNeigbors' _   _    acc                       []     = acc
+  deepNeigbors' dir maps acc@(normal,walls,fishes) (x:xs) = 
+   let (isW,isF) = isWall_isFish' obMap x
+       childs    = neighbors dir maps x
+       childs'   =  childs \\ (flattenAcc acc)
+       childs'' 
+        | isW || isF = filter (not . isWallOrFish' obMap) $ childs'
+        | otherwise  = childs'
+       acc'
+        | isW       = (  normal,x:walls,  fishes)
+        | isF       = (  normal,  walls,x:fishes) 
+        | otherwise = (x:normal,  walls,  fishes) 
+    in deepNeigbors' dir maps acc' (childs''++xs)
+
+  flattenAcc :: ([a],[a],[a]) -> [a]
+  flattenAcc (normal,walls,fishes) = fishes ++ ( walls ++ normal )
+--}
+
+
+halfDeepNeigbors2 :: Dir -> Maps -> ObId -> ([ObId],[ObId],[ObId]) -- (normal,walls,fishes)
+halfDeepNeigbors2 dir maps@(_,obMap) firstOid 
+  = deepNeigbors' dir maps ([],[],[]) [firstOid]
+ where
+  deepNeigbors' :: Dir -> Maps -> ([ObId],[ObId],[ObId]) -> [ObId] -> ([ObId],[ObId],[ObId])
+  deepNeigbors' _   _    acc                       []     = acc
+  deepNeigbors' dir maps acc@(normal,walls,fishes) (x:xs) = 
+   let (isW,isF) = isWall_isFish' obMap x
+       childs    = neighbors dir maps x
+       childs'     
+        | (isW || isF) && (x /= firstOid) = []
+        | otherwise  = childs \\ (flattenAcc acc) 
+       acc'
+        | isW       = (  normal,x:walls,  fishes)
+        | isF       = (  normal,  walls,x:fishes) 
+        | otherwise = (x:normal,  walls,  fishes) 
+    in deepNeigbors' dir maps acc' (childs'++xs)
+
+  flattenAcc :: ([a],[a],[a]) -> [a]
+  flattenAcc (normal,walls,fishes) = fishes ++ ( walls ++ normal )
 
 
 -- MAKING SEA --
@@ -265,7 +537,7 @@ mkOb strs px = do
    '$' -> ObFix
    '1' -> ObFish SmallFish
    '2' -> ObFish BigFish 
-   _   -> ObStd
+   _   -> if isUpper px then ObSteel else ObStd
 
   shift :: [[HLine]] -> Maybe ( Pos , [[HLine]] )
   shift ss = do
@@ -368,9 +640,32 @@ isWall ob = case getObType ob of
 isWallOrFish :: Ob -> Bool
 isWallOrFish ob = isWall ob || isFish ob
 
+isWall_isFish :: Ob -> (Bool,Bool)
+isWall_isFish ob = ( isWall ob , isFish ob )
+
 adjustPos :: Dir -> Ob -> Ob
 adjustPos dir (Ob t sh sigs pos px) 
  = Ob t sh sigs (pos `plus2D` dirDelta dir ) px
+
+killFish :: Ob -> Ob
+killFish (Ob (ObFish _) sh sigs pos px ) 
+ = Ob (ObFish DeadFish) sh sigs pos px
+killFish _ = error "Killing not-fish is illegal!"
+
+isDeadFish :: Ob -> Bool
+isDeadFish ob = case getObType ob of
+ ObFish DeadFish -> True
+ _ -> False
+
+isBigFish :: Ob -> Bool
+isBigFish ob = case getObType ob of
+ ObFish BigFish -> True
+ _ -> False
+
+isSteel :: Ob -> Bool
+isSteel ob = case getObType ob of 
+ ObSteel -> True
+ _ -> False
 
 
 -- General over ObId ---
@@ -381,13 +676,20 @@ getOb obMap oid = fromJust $ Map.lookup oid obMap
 pxByObId :: ObMap -> ObId -> Px
 pxByObId obMap = getPx . (getOb obMap)
 
-isFish' , isWall' , isWallOrFish' :: ObMap -> ObId -> Bool
 isFish'       = liftToObId isFish 
 isWall'       = liftToObId isWall  
 isWallOrFish' = liftToObId isWallOrFish
+isWall_isFish'= liftToObId isWall_isFish
+isBigFish'    = liftToObId isBigFish
+isSteel'      = liftToObId isSteel
 
 liftToObId :: (Ob -> a) -> (ObMap -> ObId -> a)
 liftToObId f obMap = f . (getOb obMap)
+
+-- general over sea --
+
+isGameOver :: Sea -> Bool
+isGameOver sea = GameOver == getGameStatus sea
 
 -- GENERAL GENERAL --
 
@@ -424,6 +726,21 @@ toInts = map $ concatMap $ \ (s,f) -> [s..f]
 inChars :: [String] -> [Char]
 inChars strs = sort $ (nub $ concat strs) \\ [' ']
 
+toDir :: Cmd -> [Either () Dir]
+toDir cmd = case cmd of
+ S -> [Left ()]
+ _ -> map Right $ toDir' cmd
+
+toDir' :: Cmd -> [Dir]
+toDir' cmd = case cmd of
+ U -> [DUp   ]
+ D -> [DDown ]
+ L -> [DLeft ]
+ R -> [DRight]
+ Us n -> replicate n DUp
+ Ds n -> replicate n DDown
+ Ls n -> replicate n DLeft
+ Rs n -> replicate n DRight
 
 
 -- SHOWING --
@@ -442,7 +759,10 @@ showSea sea@(Sea posMap obMap _  ((x1,y1),(x2,y2)) _ status)
         let obId = Map.lookup (x,y) posMap ,
         let px = case obId of 
                   Nothing -> ' ' 
-                  Just oid -> let Just (Ob _ _ _ _ px) = Map.lookup oid obMap in px ] ++ "\n" |
+                  Just oid -> 
+                   let Just ob@(Ob _ _ _ _ px) = Map.lookup oid obMap 
+                    in if isDeadFish ob then '#' else px 
+             ] ++ "\n" |
        my <- [(-y2)..(-y1)] , 
        let y = -my]
 
