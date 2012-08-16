54790adc670fd492ebf0f8ed35f333c80f602f85
Tom Kren <tomkren@gmail.com>
2012-08-16 02:00:20 +0200

    GP classes

--- GPclasses.hs
+++ GPclasses.hs
@@ -6,77 +6,108 @@ import System.Random
 import Control.Monad.State
 
 
-class Generable from by to where
- generateIt :: (RandomGen g) => g -> from -> by -> ( [to] , g )
+class Generable_old from by to where
+ generateIt_old :: (RandomGen g) => g -> from -> by -> ( [to] , g )
 
-class Mutable a by where
- mutateIt :: (RandomGen g) => g -> a -> by -> ( a , g )
+class Mutable_old a by where
+ mutateIt_old :: (RandomGen g) => g -> a -> by -> ( a , g )
 
-class Xoverable a where
- xoverIt :: (RandomGen g) => g-> a -> a -> ( (a,a) , g )
+class Xoverable_old a where
+ xoverIt_old :: (RandomGen g) => g-> a -> a -> ( (a,a) , g )
 
 
 type Rand a = State StdGen a
 
-getRandom :: Random a => Rand a
-getRandom = do
+randLift :: Random a => (StdGen -> (a,StdGen)) -> Rand a
+randLift f = do
  gen <- get
- let (val,gen') = random gen
+ let (val,gen') = f gen
  put gen'
  return val
+
+getRandom :: Random a => Rand a
+getRandom = randLift random
+
+getRandomR :: Random a => (a,a) -> Rand a
+getRandomR range = randLift $ randomR range
 
 mkRand :: Int -> Rand ()
 mkRand i = put $ mkStdGen i 
 
-class Generable2 from by to where
-  generateIt2 :: from -> by -> Rand [to]
+class Generable from by to where
+  generateIt :: from -> by -> Rand [to]
 
-class Mutable2 a by where
- mutateIt2 :: a -> by -> Rand a 
+class Mutable a by where
+ mutateIt :: a -> by -> Rand a 
 
 gene :: Int -> Rand a -> a
 gene i rand = fst $ runState rand (mkStdGen i)
 
 
-type CGenomType = Int
-type CGenom     = [Bool] 
-type CMutProb   = Double
+type BitGenomType = Int
+type BitGenom     = [Bool] 
+type ProbBitMut   = Double
 
-instance Generable  CGenomType () CGenom where generateIt = cGenerate
-instance Generable2 CGenomType () CGenom where generateIt2 t () = genCGenoms t
-instance Mutable  CGenom CMutProb        where mutateIt   = cMutate 
+instance Generable_old BitGenomType () BitGenom where generateIt_old = cGenerate
+instance Mutable_old BitGenom ProbBitMut        where mutateIt_old   = cMutate 
 
+instance Generable BitGenomType () BitGenom where generateIt t () = genBitGenoms t
+instance Mutable   BitGenom ProbBitMut      where mutateIt = mutBitGenom
 
-genCGenoms :: CGenomType -> Rand [CGenom]
-genCGenoms numBits = do
- genom  <- genCGenom  numBits
- genoms <- genCGenoms numBits
+genBitGenoms :: BitGenomType -> Rand [BitGenom]
+genBitGenoms numBits = do
+ genom  <- genBitGenom  numBits
+ genoms <- genBitGenoms numBits
  return $ genom : genoms
 
-genCGenom :: CGenomType -> Rand CGenom
-genCGenom numBits 
+genBitGenom :: BitGenomType -> Rand BitGenom
+genBitGenom numBits 
  | numBits == 0 = return [] 
  | otherwise    = do
   bit  <- getRandom
-  rest <- genCGenom (numBits-1)
+  rest <- genBitGenom (numBits-1)
   return $ bit : rest
 
+mutBitGenom :: BitGenom -> ProbBitMut -> Rand BitGenom
+mutBitGenom genom p = do
+ forM genom $ \ bit -> do
+  p' <- getRandomR (0.0,1.0)
+  return $ if p' < p then not bit else bit
 
 
-cGenerate :: (RandomGen g) => g -> CGenomType -> () -> ( [CGenom] , g )
+test :: (Generable typ env genom, Mutable genom mutOpt ) => Int -> typ -> env -> mutOpt -> Rand [[genom]]
+test generSize typ env mutOpt = do
+ first <- generateIt typ env
+ let first' = take generSize first
+ rest  <- mutGenerations mutOpt first'
+ return $ first' : rest
+
+mutGenerations :: (Mutable genom mutOpt ) => mutOpt -> [genom] -> Rand [[genom]]
+mutGenerations mutOpt gener = do
+ gener' <- mutGeneration  mutOpt gener 
+ rest   <- mutGenerations mutOpt gener'
+ return $ gener' : rest
+
+mutGeneration :: (Mutable genom mutOpt ) => mutOpt -> [genom] -> Rand [genom]
+mutGeneration mutOpt gener = do
+ forM gener $ \ genom -> 
+  mutateIt genom mutOpt
+
+
+cGenerate :: (RandomGen g) => g -> BitGenomType -> () -> ( [BitGenom] , g )
 cGenerate gen numBits () = 
-  let ( genom , gen'  ) = genCGenom gen  numBits 
+  let ( genom , gen'  ) = genBitGenom gen  numBits 
       ( rest  , gen'' ) = cGenerate gen' numBits ()
    in ( genom : rest , gen'' )
  where
-  genCGenom :: (RandomGen g) => g -> CGenomType -> ( [Bool] , g )
-  genCGenom gen 0       = ( [] , gen )
-  genCGenom gen numBits = 
+  genBitGenom :: (RandomGen g) => g -> BitGenomType -> ( [Bool] , g )
+  genBitGenom gen 0       = ( [] , gen )
+  genBitGenom gen numBits = 
    let ( bit  , gen'  ) = random gen
-       ( rest , gen'' ) = genCGenom gen' (numBits-1) 
+       ( rest , gen'' ) = genBitGenom gen' (numBits-1) 
     in ( bit : rest , gen'' )
 
-cMutate :: (RandomGen g) => g -> CGenom -> CMutProb -> ( CGenom , g )
+cMutate :: (RandomGen g) => g -> BitGenom -> ProbBitMut -> ( BitGenom , g )
 cMutate gen genom p = foldr f ([],gen) genom  
  where 
   f bit (acc,gen) = 
