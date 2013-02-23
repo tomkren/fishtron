fc4c7eb145fc2de76679015755de5eb398d22a33
Tom Kren <tomkren@gmail.com>
2013-02-23 20:16:32 +0100

    install bat for correct cabal installs

---
+++ v2/install.bat
@@ -0,0 +1,8 @@
+cabal update
+cabal install unix-compat-0.3.0.2 
+cabal install warp
+cabal install hint
+cabal install containers
+cabal install enumerator
+cabal install json
+cabal install normaldistribution
