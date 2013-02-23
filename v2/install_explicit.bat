a430924f4c3b120091bffb3af2cb1b1d7f2d7b30
Tom Kren <tomkren@gmail.com>
2013-02-23 20:21:28 +0100

    and second install bat with explicit versions

---
+++ v2/install_explicit.bat
@@ -0,0 +1,8 @@
+cabal update
+cabal install unix-compat-0.3.0.2 
+cabal install warp-1.3.7.2
+cabal install hint-0.3.3.4
+cabal install containers-0.5.1.0
+cabal install enumerator-0.4.19
+cabal install json-0.7
+cabal install normaldistribution-1.1.0.3
