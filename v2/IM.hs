47b7da7616a8a40ff5596b2274990369847928d0
Tom Kren <tomkren@gmail.com>
2013-03-16 23:15:48 +0100

    me im work...

--- v2/IM.hs
+++ v2/IM.hs
@@ -1,8 +1,11 @@
--- nový pokus o Inhabitation Machines bez potřeby generovat graf dopředu a vůbec to udělat pořádně
+--  Nový pokus o Inhabitation Machines bez potřeby generovat graf dopředu a vůbec to udělat pořádně
+
+--  Asi by to chtělo celý předelat tak, že ZTree bude stavová monada, ale radši 
+-- to nejdřív už dodělat když je to takle rozdělaný a až pak dyštak překopat.
 
 module IM where
 
-import TTerm (Symbol,Typ(..),Context,typeArgs)
+import TTerm (Symbol,Typ(..),TTerm(..),Context,typeArgs)
 
 import Data.List
 import Data.Maybe
@@ -11,41 +14,252 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import Data.Map (Map)
 
+import System.Random
 
-type SymbolTable = Map Typ (Set Symbol)
-type LocalVars = SymbolTable
+import qualified Data.PSQueue as Q
+
+test_head = do
+  stdGen <- newStdGen
+  let so = SearchOptions {
+    so_n                  = 5000       ,
+    so_typ                = type_head ,
+    so_ctx                = ctx_head  ,
+    so_runLen             = Nothing   ,
+    so_stdGen             = stdGen    ,
+    so_edgeSelectionModel = AllEdges  ,
+    so_randomRunState     = NoRandomRunState 
+  } 
+  let trees = proveWith so
+  mapM_ (putStrLn . show) trees
+  putStrLn $ "num terms : " ++ (show $ length trees)
+
+test_head_2 = do
+  stdGen <- newStdGen
+  let so = SearchOptions {
+    so_n                  = 100       ,
+    so_typ                = type_head ,
+    so_ctx                = ctx_head  ,
+    so_runLen             = Just 1    ,  --Nothing   ,
+    so_stdGen             = stdGen    ,
+    so_edgeSelectionModel = OneRandomEdgeWithPedicat (const True) , --AllEdges  ,
+    so_randomRunState     = NoRandomRunState 
+  } 
+  let trees = proveWith so
+  mapM_ (putStrLn . show) trees
 
 
-type Table = Map SymbolOfAtomicType (Set Entry)
-data Entry = Entry [Typ] Symbol deriving (Eq,Ord)
+data ZTree = ZTree { 
 
---instance Eq   Entry where (Entry s1 _) == (Entry s2 _) = s1 == s2
---instance Ord  Entry where (Entry s1 _) <= (Entry s2 _) = s1 <= s2
-instance Show Entry where show (Entry ts sym) = sym ++ " ... " ++ show ts
+  current     :: Tree , 
+  dads        :: [ DTree ]  ,
+
+  searchOpts  :: SearchOptions,
+  rand        :: StdGen,
+
+  locals      :: Table ,
+  globals     :: Table ,
+
+  depth       :: Int ,
+  nextVar     :: Int ,
+  numUnsolved :: Int ,
+  numSteps    :: Int ,
+  zTreeID     :: [Int]   -- pro rychlé porovnávání, mění se při expandu
+
+ } --deriving ( Eq , Ord )
 
 data Tree = TreeApp  Symbol  SymbolOfAtomicType [Tree] 
           | TreeLam [Symbol] Typ  Tree
           | TreeTyp Typ
 
-data ZTree = ZTree { 
-  current :: Tree , 
-  dads    :: [ DTree ]  ,
-
-  locals  :: Table ,
-  globals :: Table ,
-
-  depth       :: Int ,
-  nextVar     :: Int ,
-  numUnsolved :: Int
- }
-
-
 data DTree = DTreeApp [Tree] Symbol SymbolOfAtomicType [Tree]
            | DTreeLam [Symbol] Typ
 
 
-type SymbolOfAtomicType = Symbol
+type Table = Map SymbolOfAtomicType (Set Entry)
+data Entry = Entry [Typ] Symbol deriving (Eq,Ord)
 
+type SymbolOfAtomicType = Symbol
+type PriorityQueue = Q.PSQ ZTree Int
+
+
+-- snad neni nebezpečné, je tu kvuli zatřiďování do prioritní fronty
+instance Eq  ZTree where zt1 == zt2 = zTreeID zt1 == zTreeID zt2  
+instance Ord ZTree where zt1 <= zt2 = zTreeID zt1 <= zTreeID zt2
+
+
+data SearchOptions = SearchOptions{
+  so_n                  :: Int                , 
+  so_typ                :: Typ                ,  
+  so_ctx                :: Context            ,
+  so_runLen             :: Maybe Int          , -- Nothing means unlimited runLen 
+  so_stdGen             :: StdGen             ,
+  so_edgeSelectionModel :: EdgeSelectionModel ,
+  so_randomRunState     :: RandomRunState 
+ }
+
+data EdgeSelectionModel = 
+  AllEdges | 
+  OneRandomEdgeWithPedicat ((Symbol,[Typ],SymbolOfAtomicType)->Bool)
+
+data RandomRunState =
+  NoRandomRunState |
+  KozaRandomRunState IsFullMethod MaximalDepth
+
+type IsFullMethod = Maybe Bool -- Nothing značí že chci aby se vygeneroval
+type MaximalDepth = Maybe Int  -- 
+
+defaultSearchOptions :: Int -> Typ -> Context -> SearchOptions
+defaultSearchOptions n typ ctx = SearchOptions {
+  so_n                  = n ,
+  so_typ                = typ ,
+  so_ctx                = ctx ,
+  so_runLen             = Nothing ,
+  so_stdGen             = mkStdGen 42424242 ,
+  so_edgeSelectionModel = AllEdges ,
+  so_randomRunState     = NoRandomRunState
+ }
+
+proveN :: Int -> Typ -> Context -> [Tree]
+proveN n typ ctx = proveWith $ defaultSearchOptions n typ ctx
+
+proveWith :: SearchOptions -> [Tree]
+proveWith so = case m_runLen of
+  Nothing -> proveN_ n so
+  _       -> proveWith' so n
+ where
+  n        = so_n so
+  m_runLen = so_runLen so
+  runLen   = fromJust m_runLen
+  proveWith' :: SearchOptions -> Int -> [Tree]
+  proveWith' so0 toMake 
+   | toMake <= 0 = []
+   | otherwise   = 
+     let (so1,so2)= splitSearchOptions so0  --         <=============================
+         trees    = proveN_ (min runLen toMake) so1  
+         numTrees = length trees
+      in if numTrees < toMake then
+          trees ++ ( proveWith' so2 (toMake - numTrees) )  
+         else
+          trees 
+
+splitSearchOptions :: SearchOptions -> (SearchOptions,SearchOptions)
+splitSearchOptions so = let (g1,g2) = split (so_stdGen so) in ( so{ so_stdGen = g1 } , so{ so_stdGen = g2 } )
+
+proveN_ :: Int -> SearchOptions -> [Tree]
+proveN_ n so = map zTreeToTree . proveN' n $ (initQueue $ mkZTree so )
+ where 
+  proveN' :: Int -> PriorityQueue -> [ZTree]
+  proveN' 0 _ = []
+  proveN' n q = case proveStep q of
+    Nothing -> []
+    Just ( m_solution , q' ) -> case m_solution of
+      Nothing  -> proveN' n q'
+      Just sol -> sol : proveN' (n-1) q'
+
+
+initQueue :: ZTree -> PriorityQueue
+initQueue zt = Q.singleton zt (optimistNumStepPrediction zt)
+
+proveStep :: PriorityQueue -> Maybe ( Maybe ZTree , PriorityQueue )
+proveStep q = case Q.minView q of
+  Nothing                  -> Nothing
+  Just ( zt Q.:-> _ , q' ) -> case step zt of
+    Nothing  -> Just ( Just zt , q' )
+    Just zts -> Just ( Nothing , foldl insertZTree q' zts )
+ where
+  insertZTree :: PriorityQueue -> ZTree -> PriorityQueue
+  insertZTree q zt = Q.insert zt (optimistNumStepPrediction zt) q
+
+step :: ZTree -> Maybe [ZTree]
+step zt = nextTreeTypNode zt >>= return . map incrementNumSteps . expand
+
+incrementNumSteps :: ZTree -> ZTree
+incrementNumSteps zt = zt{ numSteps = (numSteps zt) + 1 }
+
+expand :: ZTree -> [ZTree]
+expand zt = case current zt of
+  TreeTyp typ -> case typ of
+    Typ alpha -> let (edges,zt') = getEdges zt -- <=========== důležitý místo
+                     zts = map (expandApp zt') edges 
+                  in map (\(z,i,gen)-> z{ zTreeID = i:(zTreeID z) , rand = gen } ) (zip3 zts [1..] (splitInf $ rand zt') )
+    _         -> [let z = expandLam typ zt in z{ zTreeID = 1:(zTreeID z)  }] 
+  _ -> error "expand : Only type-node can be expanded !"
+
+
+splitInf :: StdGen -> [StdGen]
+splitInf g0 = let (g1,g2) = split g0 in g1 : splitInf g2
+
+randomL :: RandomGen g => [a] -> g -> ( Maybe a , g)
+randomL [] g = (Nothing , g)
+randomL xs g = let (i,g') = randomR (0,length xs - 1) g in ( Just $ xs !! i , g' ) 
+
+
+
+getEdges :: ZTree -> ( [ (Symbol,[Typ],SymbolOfAtomicType) ] , ZTree )
+getEdges zt = case current zt of
+  TreeTyp (Typ alpha) -> 
+    let edges = (getEdges' (locals zt) alpha ) ++ (getEdges' (globals zt) alpha )
+     in case so_edgeSelectionModel (searchOpts zt) of
+         AllEdges -> (edges,zt)
+         OneRandomEdgeWithPedicat p -> 
+          let okEdges = filter p edges
+              ( m_edge , gen' ) = randomL okEdges (rand zt)
+           in ( maybeToList m_edge , zt{ rand = gen' } )
+  _ -> error "getEdges : Only applicable in atomic-type-node !"
+
+getEdges' :: Table -> SymbolOfAtomicType -> [ (Symbol,[Typ],SymbolOfAtomicType) ]
+getEdges' table alpha = case Map.lookup alpha table of
+  Nothing -> []
+  Just entrySet -> map (\(Entry ts sym)->(sym,ts,alpha)) (Set.toAscList entrySet)
+
+expandApp :: ZTree -> (Symbol,[Typ],SymbolOfAtomicType) -> ZTree
+expandApp zt edge = 
+  let (tree,deltaUnsolved) = expandApp' edge
+   in zt{ current = tree , numUnsolved = (numUnsolved zt) + deltaUnsolved }
+
+expandApp' :: (Symbol,[Typ],SymbolOfAtomicType) -> (Tree,Int)
+expandApp' (s,ts,alpha) = ( TreeApp s alpha (map TreeTyp ts) , (length ts) - 1 )  
+
+expandLam :: Typ -> ZTree -> ZTree
+expandLam typ zt = 
+  let (tree,i) = expandLam' (nextVar zt) (typeArgs typ)
+   in zt{ current = tree , nextVar = i }
+
+expandLam' :: Int -> ([Typ],SymbolOfAtomicType) -> (Tree,Int)
+expandLam' i (ts,alpha) = 
+   let n = length ts
+       ( ss , typ , _ ) = foldr f ([],Typ alpha,i+n-1) ts
+    in ( TreeLam ss typ (TreeTyp (Typ alpha)) , i+n )
+ where
+  f :: Typ -> ([Symbol],Typ,Int) -> ([Symbol],Typ,Int)
+  f typ1 (ss,typ2,i) = ( ('x' : show i) : ss , typ1 :-> typ2 , i-1 )
+
+
+
+mkZTree :: SearchOptions -> ZTree
+mkZTree so    = ZTree                  { 
+  current     = TreeTyp (so_typ so)    , 
+  dads        = []                     ,  
+  locals      = emptyTable             , 
+  globals     = ctxToTable (so_ctx so) , 
+  depth       = 0                      , 
+  nextVar     = 0                      ,
+  numUnsolved = 1                      ,
+  numSteps    = 0                      ,
+  zTreeID     = []                     ,
+  searchOpts  = so                     ,
+  rand        = so_stdGen so           
+ } 
+
+zTreeToTree :: ZTree -> Tree
+zTreeToTree zt = current $ goTop zt
+
+--treeToTTerm :: Tree -> TTerm
+--treeToTTerm t = case t of
+--  TreeApp s alpha ts -> 
+
+optimistNumStepPrediction :: ZTree -> Int
+optimistNumStepPrediction zt = (numSteps zt) + (numUnsolved zt) 
 
 emptyTable :: Table
 emptyTable = Map.empty
@@ -69,92 +283,10 @@ subFromTable table ss typ = addToTableWith (Set.\\)  table (zip ss (fst $ typeAr
 
 
 
-
-
-emptyST :: SymbolTable
-emptyST = Map.empty
-
-addToSTWith :: (Set Symbol->Set Symbol->Set Symbol) -> SymbolTable -> Context -> SymbolTable
-addToSTWith op z ctx = foldr (\(s,t) acc -> Map.insertWith op t (Set.singleton s) acc ) z ctx
-
-ctxToST :: Context -> SymbolTable
-ctxToST ctx = addToSTWith Set.union emptyST ctx
-
-addToLocalVars :: LocalVars -> [Symbol] -> Typ -> LocalVars
-addToLocalVars localVars ss typ = addToSTWith Set.union localVars (zip ss (fst $ typeArgs typ))
-
-subFromLocalVars :: LocalVars -> [Symbol] -> Typ -> LocalVars
-subFromLocalVars localVars ss typ = addToSTWith (Set.\\) localVars (zip ss (fst $ typeArgs typ))
-
-
-step :: ZTree -> Maybe [ZTree]
-step zt = nextTreeTypNode zt >>= return . expand
-
-expand :: ZTree -> [ZTree]
-expand zt = case current zt of
-  TreeTyp typ -> case typ of
-    Typ alpha -> map (expandApp zt) (getEdges zt)
-    _         -> [expandLam typ zt] 
-  _ -> error "expand : Only type-node can be expanded !"
-
-
-getEdges :: ZTree -> [ (Symbol,[Typ],SymbolOfAtomicType) ]
-getEdges zt = case current zt of
-  TreeTyp (Typ alpha) -> (getEdges' (locals zt) alpha ) ++ (getEdges' (globals zt) alpha ) 
-  _ -> error "getEdges : Only applicable in atomic-type-node !"
-
-getEdges' :: Table -> SymbolOfAtomicType -> [ (Symbol,[Typ],SymbolOfAtomicType) ]
-getEdges' table alpha = case Map.lookup alpha table of
-  Nothing -> []
-  Just entrySet -> map (\(Entry ts sym)->(sym,ts,alpha)) (Set.toAscList entrySet)
-
-
-expandApp :: ZTree -> (Symbol,[Typ],SymbolOfAtomicType) -> ZTree
-expandApp zt edge = 
-  let (tree,deltaUnsolved) = expandApp' edge
-   in zt{ current = tree , numUnsolved = (numUnsolved zt) + deltaUnsolved }
-
-expandApp' :: (Symbol,[Typ],SymbolOfAtomicType) -> (Tree,Int)
-expandApp' (s,ts,alpha) = ( TreeApp s alpha (map TreeTyp ts) , (length ts) - 1 )  
-
-
-expandLam :: Typ -> ZTree -> ZTree
-expandLam typ zt = 
-  let (tree,i) = expandLam' (nextVar zt) (typeArgs typ)
-   in zt{ current = tree , nextVar = i }
-
-expandLam' :: Int -> ([Typ],SymbolOfAtomicType) -> (Tree,Int)
-expandLam' i (ts,alpha) = 
-   let n = length ts
-       ( ss , typ , _ ) = foldr f ([],Typ alpha,i+n-1) ts
-    in ( TreeLam ss typ (TreeTyp (Typ alpha)) , i+n )
- where
-  f :: Typ -> ([Symbol],Typ,Int) -> ([Symbol],Typ,Int)
-  f typ1 (ss,typ2,i) = ( ('x' : show i) : ss , typ1 :-> typ2 , i-1 )
-
-
-
-
-mkZTree :: Context -> Typ -> ZTree
-mkZTree ctx typ = ZTree        { 
-  current     = TreeTyp typ    , 
-  dads        = []             ,  
-  locals      = emptyTable     , 
-  globals     = ctxToTable ctx , 
-  depth       = 0              , 
-  nextVar     = 0              ,
-  numUnsolved = 1
- } 
-
-
-
 nextTreeTypNode :: ZTree -> Maybe ZTree 
 nextTreeTypNode zt = case current zt of 
   TreeTyp _ -> Just zt
   _         -> dfsNext zt >>= nextTreeTypNode
-
-
-
 
 dfsNext :: ZTree -> Maybe ZTree
 dfsNext zt = case current zt of
@@ -168,6 +300,8 @@ dfsNext zt = case current zt of
       Nothing -> goUp zt >>= dfsNextRight
       x -> x
 
+
+
 rightBrother :: ZTree -> Maybe ZTree
 rightBrother zt = case dads zt of
   [] -> Nothing
@@ -177,8 +311,6 @@ rightBrother zt = case dads zt of
     DTreeApp leftBrothers s typ (thatBrother:rightBrothers) ->
       Just $ zt{ current = thatBrother , 
                  dads    = (DTreeApp ( current zt :leftBrothers) s typ rightBrothers )  : ancestors  }
-
-
 
 goUp :: ZTree -> Maybe ZTree
 goUp zt = case dads zt of
@@ -197,7 +329,6 @@ goUp zt = case dads zt of
   reverseConcat []     ys = ys
   reverseConcat (x:xs) ys = reverseConcat xs (x:ys)
 
-
 goDown :: ZTree -> Maybe ZTree
 goDown zt = case current zt of
   TreeApp s  alpha (t:ts) -> Just $ zt{ current = t , dads = (DTreeApp [] s alpha ts):(dads zt) , depth = (depth zt) + 1 }
@@ -205,19 +336,67 @@ goDown zt = case current zt of
                                         locals  = addToTable (locals zt) ss typ } 
   _                       -> Nothing
 
+goTop :: ZTree -> ZTree
+goTop zt = case goUp zt of
+  Nothing  -> zt
+  Just zt' -> goTop zt'
 
 
 
+instance Show Entry where show (Entry ts sym) = sym ++ " ... " ++ show ts
 
-h0 = Just $ mkZTree ctx_head $ l_int :-> m_int
-h1 = h0 >>= step >>= return . head 
-h2 = h1 >>= step >>= return . last 
-h3 = h2 >>= step >>= return . head
-h4 = h3 >>= step >>= return . head 
-h5 = h4 >>= step >>= return . head
-h6 = h5 >>= step >>= return . head . tail
-h7 = h6 >>= step >>= return . head 
-h8 = h7 >>= step
+instance Show Tree where
+  show (TreeTyp t)       = show t
+  show (TreeApp s  _ []) = s 
+  show (TreeApp s  _ ts) = "(" ++ s ++ " " ++ (fillSpaces ts) ++ ")" 
+  --show (TreeLam ss _ t ) = "(\\ " ++ (intercalate " " ss) ++ " . " ++ show t ++ ")"  
+  show (TreeLam ss typ t ) = "(\\ " ++ (showLamHead ss typ) ++ " . " ++ show t ++ ")"  
+   where
+    showLamHead :: [Symbol] -> Typ -> String
+    showLamHead ss typ = 
+      let (ts,_) = typeArgs typ
+       in intercalate " " . map (\(s,t)-> s ++ ":" ++ (show t) ) $ zip ss ts
+
+instance Show ZTree where
+  show zt = "\n----------------------------\n\n"++
+            ( foldl f (" { " ++ (show $ current zt) ++ " } ") (dads zt) ) ++ "\n" ++ 
+            "\n zTreeID     : " ++ show (zTreeID zt)++
+            "\n numSteps    : " ++ show (numSteps zt)++
+            "\n numUnsolved : " ++ show (numUnsolved zt)++
+            "\n nextVar     : " ++ show (nextVar zt) ++
+            "\n depth       : " ++ show (depth zt) ++
+            "\nlocals:\n" ++ showTable (locals  zt) ++
+            "globals:\n"  ++ showTable (globals zt) 
+   where 
+    f :: String -> DTree -> String
+    f str dt = case dt of
+      DTreeApp ts1 s _ ts2 -> "(" ++ s ++ " " ++ (fillSpaces ts1) ++ str ++ (fillSpaces ts2) ++ ")"
+      DTreeLam ss _        -> "(\\ " ++ (intercalate " " ss) ++ " . " ++ str ++ ")" 
+
+showTable :: Table -> String
+showTable table = concatMap f (Map.toAscList table)
+ where
+  f :: (SymbolOfAtomicType,Set Entry) -> String
+  f (alpha,entrySet) ="-> "++ alpha ++ "\n   " ++ (intercalate "\n   " . map show $ (Set.toAscList entrySet))  ++ "\n" 
+  
+
+fillSpaces :: Show a => [a] -> String  
+fillSpaces = intercalate " " . map show 
+
+
+
+-- examples/tests : -----------
+
+h0' = mkZTree $ defaultSearchOptions 100 type_head ctx_head 
+h0  = Just h0'
+h1  = h0 >>= step >>= return . head 
+h2  = h1 >>= step >>= return . last 
+h3  = h2 >>= step >>= return . head
+h4  = h3 >>= step >>= return . head 
+h5  = h4 >>= step >>= return . head
+h6  = h5 >>= step >>= return . head . tail
+h7  = h6 >>= step >>= return . head 
+h8  = h7 >>= step
 
 hs = map fromJust [h0,h1,h2,h3,h4,h5,h6,h7] 
 
@@ -238,62 +417,17 @@ t_2 = TreeLam ["x","y"] (Typ "A" :-> Typ "B" :-> Typ "C")
 
 --t1 = mkZTree ctx_head t_2
 
+
 int :: Typ
 int   = Typ "Int"
 m_int = Typ "MaybeInt"
 l_int = Typ "[Int]"
 
+type_head :: Typ
+type_head = l_int :-> m_int
 
 ctx_head :: Context
 ctx_head = [  ( "listCase" , l_int :-> m_int :-> (int:->l_int:->m_int) :-> m_int ),
               ( "Nothing"  , m_int ),
               ( "Just"     , int :-> m_int ) ]
-
-
-
-instance Show Tree where
-  show (TreeTyp t)       = show t
-  show (TreeApp s  _ []) = s 
-  show (TreeApp s  _ ts) = "(" ++ s ++ " " ++ (fillSpaces ts) ++ ")" 
-  --show (TreeLam ss _ t ) = "(\\ " ++ (intercalate " " ss) ++ " . " ++ show t ++ ")"  
-  show (TreeLam ss typ t ) = "(\\ " ++ (showLamHead ss typ) ++ " . " ++ show t ++ ")"  
-   where
-    showLamHead :: [Symbol] -> Typ -> String
-    showLamHead ss typ = 
-      let (ts,_) = typeArgs typ
-       in intercalate " " . map (\(s,t)-> s ++ ":" ++ (show t) ) $ zip ss ts
-
-instance Show ZTree where
-  show zt = "\n----------------------------\n\n"++
-            ( foldl f (" { " ++ (show $ current zt) ++ " } ") (dads zt) ) ++ "\n" ++ 
-            "\nnumUnsolved : " ++ show (numUnsolved zt)++
-            "\nnextVar     : " ++ show (nextVar zt) ++
-            "\ndepth       : " ++ show (depth zt) ++
-            "\nlocals:\n" ++ showTable (locals  zt) ++
-            "globals:\n"  ++ showTable (globals zt) ++
-            "-----------------------------\n"
-   where 
-    f :: String -> DTree -> String
-    f str dt = case dt of
-      DTreeApp ts1 s _ ts2 -> "(" ++ s ++ " " ++ (fillSpaces ts1) ++ str ++ (fillSpaces ts2) ++ ")"
-      DTreeLam ss _        -> "(\\ " ++ (intercalate " " ss) ++ " . " ++ str ++ ")" 
-
-showTable :: Table -> String
-showTable table = concatMap f (Map.toAscList table)
- where
-  f :: (SymbolOfAtomicType,Set Entry) -> String
-  f (alpha,entrySet) ="-> "++ alpha ++ "\n   " ++ (intercalate "\n   " . map show $ (Set.toAscList entrySet))  ++ "\n" 
-  
-showSymbolTable :: SymbolTable -> String
-showSymbolTable localVars = concatMap f (Map.toAscList localVars)
- where
-  f :: (Typ,Set Symbol) -> String
-  f (typ,symSet) = show typ ++ "  :  " ++ (intercalate " " (Set.toAscList symSet))  ++ "\n" 
-
-fillSpaces :: Show a => [a] -> String  
-fillSpaces = intercalate " " . map show 
-
-
-
--- probably redundant : -----------
 
