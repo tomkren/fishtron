72740601b700fb353c03b183a88133a9f9bb1621
Tom Kren <tomkren@gmail.com>
2013-05-06 16:02:03 +0200

    some preparations in v3

--- v3/Eva.hs
+++ v3/Eva.hs
@@ -6,3 +6,19 @@ import qualified Data.Map.Strict as Map
 
 import System.Random
 import Control.Monad.State
+import Data.Typeable
+
+import Heval (hevalsWith)
+
+
+type Eva = StateT EvaState IO
+
+data EvaState = EvaState {
+     evaGen   :: StdGen ,
+     evaJobID :: Int 
+  }
+
+
+
+evalsWith :: (Typeable a) => String -> [String] -> a -> Eva [a]
+evalsWith file strs as = liftIO $ hevalsWith file strs as
