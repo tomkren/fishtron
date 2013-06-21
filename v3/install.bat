102b387234b345d5cf6beabc2f96da498d57ea8e
Tomáš Křen <tomkren@gmail.com>
2013-06-21 16:00:08 +0200

    new install bat (for new haskell platform)
    neobsahuje něco zbytečnýho z toho starýho

---
+++ v3/install.bat
@@ -0,0 +1,9 @@
+cabal update
+cabal install warp
+cabal install hint
+cabal install containers
+cabal install enumerator
+cabal install json
+cabal install normaldistribution
+cabal install statistics-linreg
+cabal install PSQueue
