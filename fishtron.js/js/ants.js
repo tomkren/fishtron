289757b9524a22226d8e5e382b35b6ed2c655f64
Tom Kren <tomkren@gmail.com>
2013-10-15 13:10:41 +0200

    ants

--- fishtron.js/js/ants.js
+++ fishtron.js/js/ants.js
@@ -2,54 +2,216 @@
 // --- obecný mravenci -------------------
 
 var defaultOpts = {
-  rho : 0.15 ,
-
+  rho          : 0.15 ,
+  alpha        : 1.2  ,
+  beta         : 1.2  ,
+  antsPerRound : 10   ,
 };
 
 
-function updateFeroMap( feroMap , antPaths , fitness , opts ){
+function round( from , tau , succsFun , heur , isGoal , fitness , opts ){
 
-  var ret = {};
+  var antPaths = [];
 
-
-  for( var prop in feroMap ){
-    ret[prop] = feroMap[prop] * (1-opts.rho); 
+  for( var i = 0; i < opts.antsPerRound; i++ ){
+    var path = mkPath( from , tau , succsFun , heur , isGoal , opts );
+    if(path !== null ){
+      antPaths.push(path);
+    }
   }
 
+  return updateTau( tau , antPaths , fitness , opts );
+}
+
+
+function updateTau( oldTau , antPaths , fitness , opts ){
+
+  var newTau = {};
+
+  var i,j;
+
+  for( i in feroMap ){
+    for( j in feroMap ){
+      newTau[i][j] = oldTau[i][j] * (1-opts.rho); 
+    }
+  }
 
   for( var k = 0 ; k < antPaths.length ; k++ ){
 
-    var pathLen;
-    var fitVal = fitness(antPaths[k]);
+    var pathLen = antPaths[k].length;
+    var fitVal  = fitness(antPaths[k]);
 
-    for( var s = 0 ; antPaths[k].length-1 ; s ++ ){
+    for( var s = 0 ; s < pathLen-1 ; s ++ ){
 
-      var i = antPaths[k][s];
-      var j = antPaths[k][s+1];
+      i = antPaths[k][s];
+      j = antPaths[k][s+1];
 
-      ret[ij(i,j)] += fitVal ;
+      newTau[i][j] += fitVal ;
 
     }
 
   }
 
-  return ret;
-
-  //return (1-opts.rho)*oldTau + sum(deltaTaus); 
-
-}
-
-function ij(i,j){
-  return i+','+j ;
+  return newTau;
 }
 
 
+
+
+
+
+function initTau( obj , val ){
+  var tau = {};
+  var i,j;
+  for( i in obj ){
+    if( tau[i] === undefined ){ tau[i] = {}; }
+    for( j in obj[i] ){
+      tau[i][j] = val; 
+    }
+  }
+  return tau;
+}
+
+
+function selectSucc( from , path , tau , succsFun , heur , opts ){
+
+  var ss = succsFun(from,path);
+
+  if( ss.length === 0 ){ return null; }
+
+  var ps = [];
+
+  var pSum = 0;
+  var i;
+
+  for( i = 0 ; i < ss.length ; i++ ){
+    var to = ss[i];
+    var probab = Math.pow( tau[from][to], opts.alpha) * Math.pow( heur(from,to), opts.beta);
+    ps.push( probab );
+    pSum += probab;
+  }
+
+  var ball = Math.random() * pSum;
+  var nowSum = 0;
+
+  for( i = 0 ; i < ps.length ; i++ ){
+    nowSum += ps[i];
+    if( ball < nowSum ){
+      break;
+    }
+  }
+
+  return ss[i]; //[ss[i],i,ball,ps,ss,pSum];
+}
+
+function mkPath( from , tau , succsFun , heur , isGoal , opts ){
+  var next = from;
+  var path = [];
+
+  while( !isGoal(path) && next !== null ){
+
+    path.push(next);
+    var next = selectSucc( next , path , tau , succsFun , heur , opts );  
+    
+  }
+
+  return isGoal(path) ? path : null ;
+
+  
+}
 
 
 
 // ------ TSP --------------
 
-var tspInstance = mkTSPInstance({
+
+function mkTspHeur( tsp ){
+  return function(i,j){
+    if( tsp[i][j] === undefined ){ return 0; }
+    return 1 / tsp[i][j] ; 
+  }
+}
+
+function mkTspSuccs( tsp ){
+
+  return function(from,path){
+    var ret = [];
+
+    for( var to in tsp[from] ){
+
+      var obsahuje = false;
+      for(var s = 0; s < path.length ; s++){
+        if( path[s] === to){
+          obsahuje = true;
+        }
+      }
+
+      if(!obsahuje){
+        ret.push(to);
+      }
+
+    }
+
+    return ret;
+  };
+}
+
+function mkTspIsGoal( tsp ){
+  var size = _.size(tsp);
+  return function(path){
+    return path.length === size ;
+  }
+}
+
+function mkTSPInstance(obj){
+
+  var graph = {};
+
+  for( var prop in obj ){    
+    var ps = prop.split(',');
+
+    var i = ps[0];
+    var j = ps[1];
+
+    if( graph[i] === undefined ){
+      graph[i] = {};      
+    }
+    graph[i][j] = obj[prop];
+
+    if( graph[j] === undefined ){
+      graph[j] = {};      
+    }
+
+    if( graph[j][i] === undefined ){
+      graph[j][i] = graph[i][j];  
+    }
+
+  }
+
+  return graph;
+}
+
+
+
+
+
+function mkTSPFitness(tsp,Q){
+  return function(path){
+    var sum = 0;
+    for( var s = 0 ; s < path.length-1 ; s++ ){
+      var i = path[s];
+      var j = path[s+1];
+      sum += tsp[i][j];
+    }
+
+    sum += tsp[ path[path.length-1] ][ path[0] ];
+
+    return Q / sum;
+  };
+}
+
+
+var tsp1 = mkTSPInstance({
   'Praha,Londýn'  : 1034 ,
   'Praha,Berlín'  : 280  ,
   'Berlín,Londýn' : 929  ,
@@ -58,32 +220,13 @@ var tspInstance = mkTSPInstance({
   'Paříž,Londýn'  : 340
 });
 
-function mkTSPInstance(obj){
-  var ret = {};
-  for( var prop in obj ){
-    ret[prop] = obj[prop]; 
-    
-    var ps = prop.split(',');
-    var revProp = ps[1] +','+ ps[0];
-    if(obj[revProp] === undefined){
-      ret[revProp] = obj[prop];
-    }
-  }
-  return ret;
-}
+var tsp1_FF     = mkTSPFitness(tsp1,1);
 
+var tsp1_heur   = mkTspHeur( tsp1 ); 
 
+var tsp1_succs  = mkTspSuccs( tsp1 );
 
+var tsp1_tau    = initTau(tsp1,1);
 
-
-function mkTSPFitness(tsp){
-  return function(path){
-
-  };
-}
-
-
-
-
-
+var tsp1_isGoal = mkTspIsGoal(tsp1);
 
